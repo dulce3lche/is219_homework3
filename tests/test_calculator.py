9a4e3b9ea78954bd9da345c1a576d0bb742fba46
dulche3lche <br255@njit.edu>
2024-02-14 22:33:46 -0500

    updated test_calculator file to correct basic calculator

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,18 +1,11 @@
 '''My Calculator Test'''
-from calculator import Calculator
+from calculator import add, subtract
 
 def test_addition():
     '''Test that addition function works '''    
-    assert Calculator.add(2,2) == 4
+    assert add(2,2) == 4
 
 def test_subtraction():
     '''Test that addition function works '''    
-    assert Calculator.subtract(2,2) == 0
-
-def test_multiply():
-    '''Test that addition function works '''    
-    assert Calculator.multiply(2,2) == 4
-
-def test_divide():
-    '''Test that addition function works '''    
-    assert Calculator.divide(2,2) == 1
+    assert subtract(2,2) == 0
+    
