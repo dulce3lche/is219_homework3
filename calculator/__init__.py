d413047a8983b6f71fc5a096006d52a557e71d3e
dulche3lche <br255@njit.edu>
2024-07-21 22:07:58 -0400

    added decimal results to the methods and imported callable and decimal

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,20 +1,22 @@
-from calculator.calculation import Calculation
-from calculator.operations import add, subtract, multiply, divide
+from calculator.calculation import Calculation # Manages history of calculations
+from calculator.operations import add, subtract, multiply, divide # Arithmetic operations
+from typing import Callable # For type hinting callable objects
+from decimal import Decimal # For precise decimal arithmetic
 
 class Calculator:
     @staticmethod
-    def add(a,b):
+    def add(a: Decimal,b: Decimal) -> Decimal:
         calculation = Calculation(a, b, add)  # Pass the add function from calculator.operations
         return calculation.get_result()
     @staticmethod
-    def subtract(a,b):
+    def subtract(a: Decimal,b: Decimal) -> Decimal:
         calculation = Calculation(a, b, subtract)  # Pass the add function from calculator.operations
         return calculation.get_result()
     @staticmethod
-    def multiply (a,b):
+    def multiply (a: Decimal,b: Decimal) -> Decimal:
         calculation = Calculation(a, b, multiply)  # Pass the add function from calculator.operations
         return calculation.get_result()
     @staticmethod
-    def divide(a,b):
+    def divide(a: Decimal,b: Decimal) -> Decimal:
         calculation = Calculation(a, b, divide)  # Pass the add function from calculator.operations
         return calculation.get_result()
